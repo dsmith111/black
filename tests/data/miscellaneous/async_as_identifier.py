2ce52a0480bb71ffcddf4b9e2283a0d90259330b
blk-jd <davidcsmith12@gmail.com>
2025-05-15 22:07:33 -0400

    chore: noise commit #47 – touch files

--- tests/data/miscellaneous/async_as_identifier.py
+++ tests/data/miscellaneous/async_as_identifier.py
@@ -1,50 +1,50 @@
-def async(): 
-    pass 
- 
- 
-def await(): 
-    pass 
- 
- 
-await = lambda: None 
-async = lambda: None 
-async() 
-await() 
- 
- 
-def sync_fn(): 
-    await = lambda: None 
-    async = lambda: None 
-    async() 
-    await() 
- 
- 
-async def async_fn(): 
-    await async_fn() 
- 
- 
-# output 
-def async(): 
-    pass 
- 
- 
-def await(): 
-    pass 
- 
- 
-await = lambda: None 
-async = lambda: None 
-async() 
-await() 
- 
- 
-def sync_fn(): 
-    await = lambda: None 
-    async = lambda: None 
-    async() 
-    await() 
- 
- 
-async def async_fn(): 
-    await async_fn() 
-                                                                                                                                                                                                                                                                                                                                               
+def async():
+    pass
+
+
+def await():
+    pass
+
+
+await = lambda: None
+async = lambda: None
+async()
+await()
+
+
+def sync_fn():
+    await = lambda: None
+    async = lambda: None
+    async()
+    await()
+
+
+async def async_fn():
+    await async_fn()
+
+
+# output
+def async():
+    pass
+
+
+def await():
+    pass
+
+
+await = lambda: None
+async = lambda: None
+async()
+await()
+
+
+def sync_fn():
+    await = lambda: None
+    async = lambda: None
+    async()
+    await()
+
+
+async def async_fn():
+    await async_fn()
+                                                                                                                                                                                                                                                                                                                                              
