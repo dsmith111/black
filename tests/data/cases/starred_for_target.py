c71856fbc60aebd01cb8590b455767aaa90ab62e
blk-jd <davidcsmith12@gmail.com>
2025-05-15 22:07:54 -0400

    chore: noise commit #47 – touch files

--- tests/data/cases/starred_for_target.py
+++ tests/data/cases/starred_for_target.py
@@ -1,29 +1,29 @@
-# flags: --minimum-version=3.10 
-for x in *a, *b: 
-    print(x) 
- 
-for x in a, b, *c: 
-    print(x) 
- 
-for x in *a, b, c: 
-    print(x) 
- 
-for x in *a, b, *c: 
-    print(x) 
- 
-async for x in *a, *b: 
-    print(x) 
- 
-async for x in *a, b, *c: 
-    print(x) 
- 
-async for x in a, b, *c: 
-    print(x) 
- 
-async for x in ( 
-    *loooooooooooooooooooooong, 
-    very, 
-    *loooooooooooooooooooooooooooooooooooooooooooooooong, 
-): 
-    print(x) 
-                                                                                                                                                                                                                                                                                                                                                                                              
+# flags: --minimum-version=3.10
+for x in *a, *b:
+    print(x)
+
+for x in a, b, *c:
+    print(x)
+
+for x in *a, b, c:
+    print(x)
+
+for x in *a, b, *c:
+    print(x)
+
+async for x in *a, *b:
+    print(x)
+
+async for x in *a, b, *c:
+    print(x)
+
+async for x in a, b, *c:
+    print(x)
+
+async for x in (
+    *loooooooooooooooooooooong,
+    very,
+    *loooooooooooooooooooooooooooooooooooooooooooooooong,
+):
+    print(x)
+                                                                                                                                                                                                                                                                                                                                                                                             
--- tests/data/cases/trailing_comma.py
+++ tests/data/cases/trailing_comma.py
@@ -1,56 +1,56 @@
-e = { 
-    "a": fun(msg, "ts"), 
-    "longggggggggggggggid": ..., 
-    "longgggggggggggggggggggkey": ..., "created": ... 
-    # "longkey": ... 
-} 
-f = [ 
-    arg1, 
-    arg2, 
-    arg3, arg4 
-    # comment 
-] 
-g = ( 
-    arg1, 
-    arg2, 
-    arg3, arg4 
-    # comment 
-) 
-h = { 
-    arg1, 
-    arg2, 
-    arg3, arg4 
-    # comment 
-} 
- 
-# output 
- 
-e = { 
-    "a": fun(msg, "ts"), 
-    "longggggggggggggggid": ..., 
-    "longgggggggggggggggggggkey": ..., 
-    "created": ..., 
-    # "longkey": ... 
-} 
-f = [ 
-    arg1, 
-    arg2, 
-    arg3, 
-    arg4, 
-    # comment 
-] 
-g = ( 
-    arg1, 
-    arg2, 
-    arg3, 
-    arg4, 
-    # comment 
-) 
-h = { 
-    arg1, 
-    arg2, 
-    arg3, 
-    arg4, 
-    # comment 
-} 
-                                                                                                                                                                                                                                                                                                                                                                                              
+e = {
+    "a": fun(msg, "ts"),
+    "longggggggggggggggid": ...,
+    "longgggggggggggggggggggkey": ..., "created": ...
+    # "longkey": ...
+}
+f = [
+    arg1,
+    arg2,
+    arg3, arg4
+    # comment
+]
+g = (
+    arg1,
+    arg2,
+    arg3, arg4
+    # comment
+)
+h = {
+    arg1,
+    arg2,
+    arg3, arg4
+    # comment
+}
+
+# output
+
+e = {
+    "a": fun(msg, "ts"),
+    "longggggggggggggggid": ...,
+    "longgggggggggggggggggggkey": ...,
+    "created": ...,
+    # "longkey": ...
+}
+f = [
+    arg1,
+    arg2,
+    arg3,
+    arg4,
+    # comment
+]
+g = (
+    arg1,
+    arg2,
+    arg3,
+    arg4,
+    # comment
+)
+h = {
+    arg1,
+    arg2,
+    arg3,
+    arg4,
+    # comment
+}
+                                                                                                                                                                                                                                                                                                                                                                                             
